f2b4455276c43bf4e2ea2b9aae1dfb50a94de6fa
Harry-Bro <likeharrybro@gmail.com>
2021-10-08 08:36:16 +0900

    :heavy_plus_sign: Add junit5

--- hello-kafka-kotlin/build.gradle.kts
+++ hello-kafka-kotlin/build.gradle.kts
@@ -13,8 +13,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
     // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
     implementation("org.apache.kafka:kafka-clients:2.5.0")
+//    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    testImplementation("org.slf4j:slf4j-simple:1.7.32")
 }
 
 tasks.test {
